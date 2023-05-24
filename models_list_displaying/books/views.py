10d1f4f1f99e3ca4720158e9275781bca80883a8
Vladislav Dikanov <vladdikanov@gmail.com>
2023-05-25 01:22:24 +0700

    edit 1

--- models_list_displaying/books/views.py
+++ models_list_displaying/books/views.py
@@ -14,9 +14,18 @@ def books_view(request):
 def books_date(request,date):
     template = 'books/books_list2.html'
     books = Book.objects.filter(pub_date=date)
-    previous_page = Book.objects.filter(pub_date=date).order_by('-pub_date').first()
-    next_page = Book.objects.filter(pub_date=date).order_by('pub_date').first()
-    print(previous_page.pub_date)
-    print(next_page.pub_date)
-    context = {"books" : books}
+    context = {"books": books}
+    try:
+        previous_page = Book.objects.filter(pub_date__lt=date).order_by('-pub_date').first()
+        context["previous_page"] = str(previous_page.pub_date)
+    except:
+        context["previous_page"] = None
+    try:
+        next_page = Book.objects.filter(pub_date__gt=date).order_by('pub_date').first()
+        context["next_page"] = str(next_page.pub_date)
+    except:
+        context["next_page"] = None
+    print(context)
+    # print(previous_page.pub_date)
+    # print(next_page.pub_date)
     return render(request, template, context)
